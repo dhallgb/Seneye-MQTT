04186e7182e1d96e118f86be846cacc426f6ef7f
Doug Hall <wredgum@gmail.com>
2017-07-01 23:34:42 +0100

    bitwise operations on data

--- sudmqtt.py
+++ sudmqtt.py
@@ -6,19 +6,20 @@
 #
 import paho.mqtt.publish as publish
 import usb.core, usb.util
-import yaml, sys, json
+import yaml, sys, json, pprint
+from bitstring import BitArray
 config=[]
 device=[]
 interface=0
 
 def printhex(s):
-    print(type(s),len(s),":".join("{:02x}".format(c) for c in s))
+    return(type(s),len(s),":".join("{:02x}".format(c) for c in s))
 
 def setup_up():
     # find the device using product id strings
     dev = usb.core.find(idVendor=9463, idProduct=8708)
     if __debug__:
-        print("device       >>>",device)
+        print("device       >>>",dev)
     return(dev)
 
 def read_sud(dev, interface):
@@ -46,7 +47,7 @@ def read_sud(dev, interface):
         print("endpoint in  >>>",epIn)
         print("endpoint out >>>",epOut)
 
-    # write to device with signature string
+    # write to device with hello string
     msg="HELLOSUD"
     rc=dev.write(epOut,msg)
     if __debug__:
@@ -54,10 +55,8 @@ def read_sud(dev, interface):
 
     # read from device
     ret=dev.read(epIn,epIn.wMaxPacketSize)
-    sret = ''.join([chr(x) for x in ret])
     if __debug__:
-        print("return       >>>",ret)
-        print("return string>>>",sret)
+        print("HELO hex     >>>",printhex(ret))
 
     # write to device with reading request
     msg="READING"
@@ -65,34 +64,63 @@ def read_sud(dev, interface):
     if __debug__:
         print("READ ret code>>>",rc)
 
-    # read from device
+    # read from device twice, first for return from "READING", second for actual values
     ret=dev.read(epIn,epIn.wMaxPacketSize,1000)
-    sret = ''.join([chr(x) for x in ret])
-    if __debug__:
-        print("return       >>>",ret)
-        print("return string>>>",sret)
-
-    # read from device again, long time out
     ret=dev.read(epIn,epIn.wMaxPacketSize,10000)
-    sret = ''.join([chr(x) for x in ret])
+    c = BitArray(ret)
     if __debug__:
-        print("return       >>>",printhex(ret))
-        print("return string>>>",sret)
+        print("sensor hex   >>>",printhex(ret))
+        print("sensor bits  >>>",c.bin)
+        print("sensor bits len>",len(c.bin))
 
     # write to device with close string
     msg="BYESUD"
     rc=dev.write(epOut,msg)
     if __debug__:
         print("BYE ret code >>>",rc)
-    return(sret)
+    return(c)
 
-def mungReadings(params):
-    # strip bytes to bits
+def mungReadings(p):
+    # I start by assuming that short = 16 bits, int = 32 bits
+    #
+    #int Ts;
+    #bit reserved : 2;
+    #bit InWater : 1;
+    #bit SlideNotFitted : 1;
+    #bit SlideExpired : 1;
+    #bit StateT : 2;
+    #bit StatePh : 2;
+    #bit StateNh3 : 2;
+    #bit Error : 1;
+    #bit IsKelvin : 1;
+    #char reserved[8];   this is 64 bits long!
+    #int  Kelvin;
+    #int  x;
+    #int  y;
+    #int  Par;
+    #int  Lux;
+    #char  PUR;
+    #short pH;
+    #short Nh3;
+    #int  T; - I presume that this is temperature
+    #char reserved[16];
+    s={}
+    i=36 # this is the start of the data, so int + 2 bits = 34, right??!!?!?!
+    s['InWater']=p[i]
+    s['SlideNotFitted']=p[i+1]
+    s['SlideExpired']=p[i+2]
+    s['StateT']=p[i+3:i+4]
+    s['StatePh']=p[i+5:i+6]
+    s['StateNh3']=p[i+7:i+8]
+    s['Error']=p[i+9]
+    s['IsKelvin']=p[i+10]
+    s['Temp']=p[i+7:i+8]
+    s['pH']=p[i+7:i+8]
+    s['NH3']=p[i+7:i+8]
 
-    # build json
-    sensorReadings=[]
-    return(params)
-    return(sensorReadings)
+    if __debug__:
+        pprint.pprint(s)
+    return(None)
 
 def clean_up(dev):
     # re-attach kernel driver
